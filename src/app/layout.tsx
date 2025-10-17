ad3deca349b43e3445bd946b251d708c5ae12b14
Kian van Trigt <kianvantrigt@gmail.com>
2025-10-17 20:47:32 +0000

    Kun je het zelfde logo die in de linker hoek hebt staan ook op het tabla

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path d='M83.4 25L12.5 45.8l-3.7-10c-1.2-4.6 1.2-9.2 5.4-10.4L57.7 11.9c4.6-1.2 9.2 1.2 10.4 5.4Z' fill='%2314b8a6'/><path d='m25.8 22.1 12.9 16.2'/><path d='m51.7 15 12.9 16.7'/><path d='M12.5 45.8h75v33.4a8.3 8.3 0 0 1-8.3 8.3H20.8a8.3 8.3 0 0 1-8.3-8.3Z' fill='%2314b8a6'/></svg>" />
       </head>
       <body className="font-body bg-background text-foreground antialiased">
         <FirebaseClientProvider>
