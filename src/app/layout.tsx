d0f4c1891292639a5ab071df02f380eda9c68d22
Kian van Trigt <kianvantrigt@gmail.com>
2025-10-17 15:02:36 +0000

    File changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Header } from '@/components/layout/header';
 import { Toaster } from "@/components/ui/toaster";
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'CineVerse',
@@ -21,11 +22,13 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body bg-background text-foreground antialiased">
-        <Header />
-        <main className="container py-8">
-            {children}
-        </main>
-        <Toaster />
+        <FirebaseClientProvider>
+          <Header />
+          <main className="container py-8">
+              {children}
+          </main>
+          <Toaster />
+        </FirebaseClientProvider>
       </body>
     </html>
   );
