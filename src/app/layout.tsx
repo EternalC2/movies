eed7ba95f06835d77eb7b70bbe1bab67f2b40ce8
Kian van Trigt <kianvantrigt@gmail.com>
2025-10-21 02:05:24 +0000

    Het logo is mooi maar op mobiel doet hij het nog niet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Header } from '@/components/layout/header';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase/client-provider';
-import { Clapperboard } from 'lucide-react';
 
 export const metadata: Metadata = {
   title: 'Eternal Movies',
@@ -21,8 +20,15 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
+        
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/apple-icon.png" />
+        
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body bg-background text-foreground antialiased">
         <FirebaseClientProvider>
