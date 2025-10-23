677e155e05eab0dba9cfa13bc291b79cede09a88
Kian van Trigt <kianvantrigt@gmail.com>
2025-10-23 18:09:20 +0000

    Zet de favicon terug naar hoe het normaal werkt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,14 +20,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
-        
-        {/* These specific apple-touch-icon links are best practice for iOS devices */}
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/apple-icon.png" />
-        
-        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body bg-background text-foreground antialiased">
         <FirebaseClientProvider>
